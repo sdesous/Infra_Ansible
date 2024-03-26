053cba1b377411fbb08d5b44f56862dd043aa728
sdesous <shadddesous@gmail.com>
2024-03-26 21:25:09 +0100

    Add backup script and cron job

--- resources/backup.sh
+++ resources/backup.sh
@@ -1,50 +1,12 @@
 #!/bin/ash
 
-# Spécifiez le répertoire que vous souhaitez traiter
-backup_path="/volumes/share/Backup"
+# Specify the absolute backup path
+backup_path="/Backup_volumes/"
 
-# Déterminez la date actuelle
-current_date=$(date +%s)
+# Delete all 15 days old files
+find $backup_path -type f -mtime +15 -exec rm {} \;
 
-# Déterminez la limite de date (une semaine en secondes)
-limit_date=$((current_date - 7 * 24 * 60 * 60))
-
-# Parcourez tous les fichiers dans le répertoire
-for fichier in "$backup_path"/*; do
-    # Vérifiez si le fichier est un fichier ordinaire
-    if [ -f "$fichier" ]; then
-        # Récupérez la date de modification du fichier en secondes depuis l'époque
-        file_age=$(date -r "$fichier" +%s)
-
-        # Vérifiez si la date de modification est antérieure à la limite
-        if [ "$file_age" -lt "$limit_date" ]; then
-            # Supprimez le fichier
-            rm "$fichier"
-        fi
-    fi
-done
-
-# Déterminez tous les dossiers à sauvegarder
-service=$(ls /volumes | grep "volume" | cut -d '_' -f 1)
-
-volumes_path=''
-
-# Stockez les chemins dans un variable
-for path in $service
-do
-	volumes_path="/volumes/"$path"_volume "$volumes_path
-done
-
-# Ajoutez les cas particuliers
-volumes_path="$volumes_path /volumes/certificates /root/.wg-easy"
-
-# Créez l'archive
-tar -czf $backup_path/$(date -I).tar.gz $volumes_path
-chown root $backup_path/$(date -I).tar.gz
-chgrp root $backup_path/$(date -I).tar.gz
+# Create a new backup then change it's permissions
+tar -czf $backup_path/$(date -I).tar.gz /Nextcloud /volumes
+chown root:root $backup_path/$(date -I).tar.gz
 chmod 400 $backup_path/$(date -I).tar.gz		
-
-# tar --exclude='*.jpg' --exclude='*.png' --exclude='*.webp' -czvf /home/admin/jellyfin.tar.gz cache config transcodes
-
-# tar --exclude='*.jpg' --exclude='*.png' --exclude='*.webp' --exclude='*[Mm]etadata*' --exclude='*[Mm]ediaCover*' --exclude='volumes/transmission_volume/config/resume/*' -czvf /Backup_volumes/$(date -I).tar.gz /volumes/*
-
