b38271c12a522942eba5847f070783c5b26a16fb
sdesous <shadddesous@gmail.com>
2024-03-27 09:36:40 +0100

    Delete bunch of folders and add role template

--- resources/cert.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/ash
-
-sudo certbot certonly --manual --preferred-challenges=dns --email shadddesous@gmail.com --server https://acme-v02.api.letsencrypt.org/directory --agree-tos --manual-public-ip-logging-ok -d "*.sdesous.fr" -d "sdesous.fr" 
